4f833c6a72663c8a0fa2205c21a3d4e52104dec0
am230 <111672334+am230@users.noreply.github.com>
2023-06-27 23:17:49 +0900

    動いて欲しい

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,25 +1,44 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import {NextApiRequest, NextApiResponse} from "next";
 import normalizeUrl from 'normalize-url';
-import { encode } from "../../lib/shortener";
+import {encode} from "../../lib/shortener";
 import prisma from "../../lib/prisma";
 
 interface CreateLink {
     url: string;
 }
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
-) {
-    const { url }: CreateLink = req.body;
-    const link = await prisma.link.create({
+export interface Response {
+    id?: string
+    error?: string
+    ok: boolean
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>,) {
+    const {url}: CreateLink = req.body;
+    if (!url) {
+        return res.status(200).json({
+            ok: false, error: 'うまく入力できてないようです…',
+        });
+    }
+    let normalized;
+    try {
+        normalized = normalizeUrl(url)
+    } catch (error) {
+        return res.status(200).json({
+            ok: false, error: '意味の分からないエラーが出ました…',
+        });
+    }
+    prisma.link.create({
         data: {
-            url: normalizeUrl(url),
+            url: normalized,
         },
-    });
-    res.status(200).json({
-        id: encode(link.id),
-        createdAt: link.createdAt,
-        url: link.url,
-    });
+    }).then(link => {
+        res.status(200).json({
+            ok: true, id: encode(link.id),
+        });
+    }).catch(reason => {
+        res.status(200).json({
+            ok: false, error: '作成に失敗しました…',
+        });
+    })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,60 @@
 import axios from 'axios';
 import React from 'react';
-import { Form } from 'react-final-form';
-
-
-interface LinkResult {
-    id: string
-    createdAt: Date
-    name: string
-    url: string
-}
+import {Form} from 'react-final-form';
+import {Response} from "./api/create";
 
 const Home = () => {
     const [url, setUrl] = React.useState('');
+    const [message, setMessage] = React.useState(<p className='message'></p>);
 
-    const [message, setMessage] = React.useState('');
+    const destroyMessage = () => {
+        setMessage(<p className='message'></p>);
+    };
 
     const createLink = async () => {
-        const { data } = await axios.post<LinkResult>('/api/create', { url });
+        destroyMessage();
+        const {data} = await axios.post<Response>('/api/create', {url});
+        if (!data.ok) {
+            console.log(data);
+            setMessage(<p className='message failed'>
+                <span className="material-symbols-outlined">&#xe000;</span>
+                {data.error}
+            </p>);
+            setTimeout(() => destroyMessage(), 5000);
+            return;
+        }
         const result = `${window.location.origin}/${data.id}`
         navigator.clipboard.writeText(result);
-        setMessage('Copied!');
-        setTimeout(() => {
-            setMessage('');
-        }, 500);
+        setMessage(<p className='message'>
+            <span className="material-symbols-outlined">&#xe86c;</span>
+            コピーしました！
+        </p>);
+        setTimeout(() => destroyMessage(), 5000);
     }
 
-    return (
-        <div className='main'>
-            <Form
-                onSubmit={createLink}
-                render={({ handleSubmit }) => (
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" placeholder='url...' value={url} onChange={(e) => setUrl(e.target.value)} />
-                        <button onClick={handleSubmit}>Create & Copy</button>
-                    </form>
-                )}
-            />
-            <p className='message'>{message}</p>
-        </div >
-    )
+    return (<div className='main'>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
+        <Form
+            onSubmit={createLink}
+            render={({handleSubmit}) => (<form onSubmit={handleSubmit}>
+                1.
+                <input
+                    type="text"
+                    placeholder='@id...'
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                />
+                2.
+                <button onClick={handleSubmit}>作ってコピー</button>
+            </form>)}
+        />
+        {message}
+        <div className="bottom">
+            <a href="https://github.com/am230/blank-url">BlankUrl</a>
+            made by
+            <a href="https://twitter.com/AM4_02">二時半</a>
+        </div>
+    </div>);
 }
 
 export default Home
